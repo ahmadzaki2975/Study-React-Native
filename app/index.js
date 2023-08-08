e325a3380f7f0d824e06da40cebf8e4d3a9afc5a
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-08 14:07:00 +0700

    feat: test some styles

--- app/index.js
+++ app/index.js
@@ -1,9 +1,51 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, FONTS, SIZES, icons } from "../constants/index";
+import {
+  NearbyJobs,
+  PopularJobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.lightWhite,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: true,
+          headerLeft: () => {
+            return <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />;
+          },
+          headerRight: () => {
+            return <ScreenHeaderBtn iconUrl={icons.search} dimension="100%" />;
+          },
+          headerTitle: () => {
+            return <></>;
+          },
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: SIZES.medium,
+        }}>
+          <Text>Ahmad Zaki Akmal</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
